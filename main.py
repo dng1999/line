7cc8f7f10060588487f32a27ef57814e4df63329
Dorothy Ng <dng1@stuy.edu>
2017-02-12 12:51:46 -0500

    updated files to reflect fixed source code

--- main.py
+++ main.py
@@ -7,7 +7,15 @@ color = [ 0, 0, 0 ]
 for i in range(0,500):
     color[0] = (i^2 + i + 20)  % 256
     color[2] = (i * 7 - 10)  % 256
-    draw_line(screen,0,0,500,i,color)
+    draw_line(0,0,500,i,screen,color)
+
+display(screen)
+clear_screen(screen)
+
+for i in range(0,500):
+    color[0] = (i^2 + i + 20)  % 256
+    color[2] = (i * 7 - 10)  % 256
+    draw_line(0,0,500,i,screen,color)
 
 display(screen)
 save_extension(screen, 'img.png')
